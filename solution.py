f0084a3b7fe95682812590cbcf92f0d2a3c6783c
progall <progall@users.noreply.github.com>
2023-04-11 16:16:32 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import pareto, cauchy, norm, ttest_ind, ks_2samp, mannwhitneyu, permutation_test
 
 chat_id = 322172960 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    y = []
+    
+    for хlst in х:
+        if xlt > 300:
+            y.append(300)
+        else
+            y.append(xlt)
+            
+    return ttest_ind(x, y, equal_var=False, alternative="greater").pvalue < 0.04 # Ваш ответ, True или False
